6daa0e71bc5606fcd4dc9b9fe200d14411bb855f
Amit Kumar Nayak <amitkumarnayak40@gmail.com>
2023-06-09 12:17:56 +0530

    [Amit kumar] Displaying Welcome to Employee Wage Computation

---
+++ Employee_Wage/Employee_Wage/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Employee_Wage
+{
+    internal class Program
+    {
+        public const int Full_Time = 1;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Employ Wage Calculation");   
+        }
+    }
+}
