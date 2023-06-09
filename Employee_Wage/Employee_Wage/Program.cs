23ff8373dd1e0ea3e245c33bfdfdd43ab5fdda4a
Amit Kumar Nayak <amitkumarnayak40@gmail.com>
2023-06-09 13:02:06 +0530

    [Amit Kumar]Added:Part time Emplayee and calculate wage

--- Employee_Wage/Employee_Wage/Program.cs
+++ Employee_Wage/Employee_Wage/Program.cs
@@ -3,20 +3,27 @@
     internal class Program
     {
         public const int Full_Time = 1;
-
+        public const int Part_Time = 2;
         public const int EmpRatePerHour = 20;
         static void Main(string[] args)
         {
             Console.WriteLine("Employ Wage Calculation");
+
             int empHrs = 0;
             int empWage = 0;
 
+
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
+
 
             if (empCheck == Full_Time)
             {
                 empHrs = 8;
+            }
+            else if (empCheck == Part_Time)
+            {
+                empHrs = 4;
             }
             else
             {
