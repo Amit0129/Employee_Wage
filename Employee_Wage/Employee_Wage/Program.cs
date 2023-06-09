01882410ab7f8156cf088fbd43b432f341864071
Amit Kumar Nayak <amitkumarnayak40@gmail.com>
2023-06-09 13:25:46 +0530

    [Amit Kumar]Added:Calculating employee wage for month (20 working Day)

--- Employee_Wage/Employee_Wage/Program.cs
+++ Employee_Wage/Employee_Wage/Program.cs
@@ -5,33 +5,36 @@
         public const int Full_Time = 1;
         public const int Part_Time = 2;
         public const int EmpRatePerHour = 20;
+        public const int NumOfWorkDay = 20;
         static void Main(string[] args)
         {
             Console.WriteLine("Employ Wage Calculation");
 
-
             int empHrs = 0;
             int empWage = 0;
+            int totalEmpWage = 0;
 
-
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-
-
-            switch (empCheck)
+            for (int day = 0; day < NumOfWorkDay; day++)
             {
-                case Full_Time:
-                    empHrs = 8;
-                    break;
-                case Part_Time:
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+
+                switch (empCheck)
+                {
+                    case Full_Time:
+                        empHrs = 8;
+                        break;
+                    case Part_Time:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EmpRatePerHour;
+                totalEmpWage += empWage;
             }
-            empWage = empHrs * EmpRatePerHour;
-            Console.WriteLine("Employ Wage is : " + empWage);
+            Console.WriteLine("Total Employ Wage is : " + totalEmpWage);
         }
     }
 }
