8ca94d5ea745d1c4307a90e885b6d6c4220028d3
Amit Kumar Nayak <amitkumarnayak40@gmail.com>
2023-06-09 12:22:23 +0530

    [Amit Kumar]Added:Employee is present or absent

--- Employee_Wage/Employee_Wage/Program.cs
+++ Employee_Wage/Employee_Wage/Program.cs
@@ -5,7 +5,17 @@
         public const int Full_Time = 1;
         static void Main(string[] args)
         {
-            Console.WriteLine("Employ Wage Calculation");   
+            Console.WriteLine("Employ Wage Calculation");
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == Full_Time)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
         }
     }
 }
